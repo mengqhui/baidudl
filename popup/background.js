936552309086f03f0f8d1034b5f7dd20b8313485
Kyle ZENG <jkjh1jkjh1@gmail.com>
2017-05-16 13:32:06 +0800

    bug fix: don't load jquery library

--- popup/background.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.executeScript({file: "content_script/inject.js"}, callback);
-
-function callback(){
-	if(chrome.runtime.lastError){
-		console.log(chrome.runtime.lastError.message);
-	}
-}
--- popup/controller.js
+++ popup/controller.js
@@ -1,8 +1,11 @@
+// initialize angular model
 var app = angular.module('app', []);
 app.controller('control', function($scope){
-	$scope.message = 'Running...';
+	$scope.message = 'Ready...';
 	$scope.status = false;
 })
+
+// add listener to handle received message 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	$scope = angular.element(document.getElementById('app')).scope();
 	$scope.$apply(function(){
@@ -15,3 +18,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	});
 	sendResponse('Success');
 })
+
+// execute content script
+chrome.tabs.executeScript({file: "content_script/inject.js"}, function(){
+	if(chrome.runtime.lastError){
+		console.log(chrome.runtime.lastError.message);
+	}
+});
